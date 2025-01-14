25dd683ded862781bc71f91e68d3c40d8944de1a
MXMaddux <maxmaddux@gmail.com>
2025-01-14 05:43:32 -0800

    Try fixing deployment errors

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,14 +3,10 @@ import ProductsContainer from "@/components/products/ProductsContainer";
 async function ProductsPage({
   searchParams,
 }: {
-  searchParams: { layout?: string; search?: string };
+  searchParams: Promise<{ layout?: string; search?: string }>;
 }) {
-  const layout = searchParams.layout || "grid";
-  const search = searchParams.search || "";
-  return (
-    <>
-      <ProductsContainer layout={layout} search={search} />
-    </>
-  );
+  const layout = (await searchParams).layout || "grid";
+  const search = (await searchParams).search || "";
+  return <ProductsContainer layout={layout} search={search} />;
 }
 export default ProductsPage;
