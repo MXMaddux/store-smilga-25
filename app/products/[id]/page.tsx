795fb5132f19930ed21dcf7100f961fcf9f7eaa0
MXMaddux <maxmaddux@gmail.com>
2025-01-15 05:58:12 -0800

    Return singleProductPage to original

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,72 +6,13 @@ import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
 
-// type SingleProductPageProps = {
-//   params: {
-//     id: string;
-//   };
-// };
-
-// async function SingleProductPage({ params }: SingleProductPageProps) {
-//   const product = await fetchSingleProduct(params.id);
-//   const { name, image, company, description, price } = product;
-//   const dollarsAmount = formatCurrency(price);
-//   return (
-//     <section>
-//       <BreadCrumbs name={product.name} />
-//       <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
-//         {/* IMAGE FIRST COL */}
-//         <div className="relative h-full">
-//           <Image
-//             src={image}
-//             alt={name}
-//             fill
-//             sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw,33vw"
-//             priority
-//             className="w-full rounded-md object-cover"
-//           />
-//         </div>
-//         {/* PRODUCT INFO SECOND COL */}
-//         <div>
-//           <div className="flex gap-x-8 items-center">
-//             <h1 className="capitalize text-3xl font-bold">{name}</h1>
-//             <FavoriteToggleButton productId={params.id} />
-//           </div>
-//           <ProductRating productId={params.id} />
-//           <h4 className="text-xl mt-2">{company}</h4>
-//           <p className="mt-3 text-md bg-muted inline-block p-2 rounded-md">
-//             {dollarsAmount}
-//           </p>
-//           <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
-//           <AddToCart productId={params.id} />
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-// export default SingleProductPage;
-
-interface Params {
-  id: string;
-}
-
-export default async function SingleProductPage({
-  params,
-}: {
-  params: Params;
-}) {
+async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id);
-
-  if (!product) {
-    throw new Error("Product not found");
-  }
-
   const { name, image, company, description, price } = product;
   const dollarsAmount = formatCurrency(price);
-
   return (
     <section>
-      <BreadCrumbs name={name} />
+      <BreadCrumbs name={product.name} />
       <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
         {/* IMAGE FIRST COL */}
         <div className="relative h-full">
@@ -102,3 +43,4 @@ export default async function SingleProductPage({
     </section>
   );
 }
+export default SingleProductPage;
