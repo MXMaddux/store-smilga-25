4fbbdc2a90de502e8f5f429b3d655061b3718c2e
MXMaddux <maxmaddux@gmail.com>
2025-01-16 04:49:23 -0800

    Add Navbar to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
 import LoadingContainer from "@/components/global/LoadingContainer";
 import { Suspense } from "react";
+import Navbar from "@/components/navbar/Navbar";
 
 function HomePage() {
   return (
     <>
+      <Navbar />
       <Hero />
       <Suspense fallback={<LoadingContainer />}>
         <FeaturedProducts />
