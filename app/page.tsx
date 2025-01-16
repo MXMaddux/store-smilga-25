d07a41243f0c645d845a8b78721b36360144f1c7
MXMaddux <maxmaddux@gmail.com>
2025-01-16 07:47:51 -0800

    Remove Nabvbar from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,10 @@ import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
 import LoadingContainer from "@/components/global/LoadingContainer";
 import { Suspense } from "react";
-import Navbar from "@/components/navbar/Navbar";
 
 function HomePage() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Suspense fallback={<LoadingContainer />}>
         <FeaturedProducts />
