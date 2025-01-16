1f294eb64b7dd14a1480c24c673874a3c6a11b60
MXMaddux <maxmaddux@gmail.com>
2025-01-16 06:20:06 -0800

    Add Environment variables in .env.local to vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
+// after instructor help
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Next Storefront",
@@ -14,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,23 @@
+"use server";
+
 import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+// import {
+//   imageSchema,
+//   productSchema,
+//   reviewSchema,
+//   validateWithZodSchema,
+// } from './schemas';
+// import { deleteImage, uploadImage } from './supabase';
+// import { revalidatePath } from 'next/cache';
+// import { Cart } from '@prisma/client';
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/");
+  return user;
+};
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -34,4 +52,17 @@ export const fetchSingleProduct = async (productId: string) => {
     redirect("/products");
   }
   return product;
+};
+
+export const fetchUserFavorites = async () => {
+  const user = await getAuthUser();
+  const favorites = await db.favorite.findMany({
+    where: {
+      clerkId: user.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+  return favorites;
 };
