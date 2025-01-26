ebecf823c8d8f4a785083bdc19da4083b2293fe4
MXMaddux <maxmaddux@gmail.com>
2025-01-26 06:43:16 -0800

    complete FavoritesPage

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,22 +1,17 @@
-// import SectionTitle from "@/components/global/SectionTitle";
-// import ProductsGrid from "@/components/products/ProductsGrid";
-// import { fetchUserFavorites } from "@/utils/actions";
+import SectionTitle from "@/components/global/SectionTitle";
+import ProductsGrid from "@/components/products/ProductsGrid";
+import { fetchUserFavorites } from "@/utils/actions";
 
-// async function FavoritesPage() {
-//   const favorites = await fetchUserFavorites();
-//   if (favorites.length === 0)
-//     return <SectionTitle text="You have no favorites yet." />;
+async function FavoritesPage() {
+  const favorites = await fetchUserFavorites();
+  if (favorites.length === 0)
+    return <SectionTitle text="You have no favorites yet." />;
 
-//   return (
-//     <div>
-//       <SectionTitle text="Favorites" />
-//       <ProductsGrid products={favorites.map((favorite) => favorite.product)} />
-//     </div>
-//   );
-// }
-// export default FavoritesPage;
-
-function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+  return (
+    <div>
+      <SectionTitle text="Favorites" />
+      <ProductsGrid products={favorites.map((favorite) => favorite.product)} />
+    </div>
+  );
 }
 export default FavoritesPage;
--- components/products/ProductsContainer.tsx
+++ components/products/ProductsContainer.tsx
@@ -62,5 +62,4 @@ async function ProductsContainer({
     </>
   );
 }
-
 export default ProductsContainer;
